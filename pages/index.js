b251138e90c37cb1075c05d8bfa2a91f9204a3d1
m7mark <m7mark@yandex.ru>
2022-02-08 01:38:32 +0300

    Try catch

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,8 @@
 import CarForm from "../lib/CarForm";
+import SearchForm from "../lib/SearchForm";
+import { Tabs } from 'antd';
+
+const { TabPane } = Tabs;
 
 export default function Home() {
   return (
@@ -7,8 +11,16 @@ export default function Home() {
         <div className="header"></div>
         <div className="tabWrapper">
           <div className="tabContainer">
-            <h1>Create a Car</h1>
-            <CarForm />
+            <Tabs size='large' defaultActiveKey="1" centered>
+              <TabPane tab="Create Car" key="1">
+                <h1>Create a Car</h1>
+                <CarForm />
+              </TabPane>
+              <TabPane tab="Search Cars" key="2">
+                <SearchForm />
+              </TabPane>
+            </Tabs>
+
           </div>
         </div>
       </div>
