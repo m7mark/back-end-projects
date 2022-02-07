3007e81c38970196126fddee43ae608eb60869d3
m7mark <m7mark@yandex.ru>
2022-02-07 17:17:37 +0300

    AntDesign

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,15 @@ import CarForm from "../lib/CarForm";
 export default function Home() {
   return (
     <>
-    <h1>Create a Car</h1>
-    <CarForm/>
+      <div className="App">
+        <div className="header"></div>
+        <div className="tabWrapper">
+          <div className="tabContainer">
+            <h1>Create a Car</h1>
+            <CarForm />
+          </div>
+        </div>
+      </div>
     </>
   )
 }
