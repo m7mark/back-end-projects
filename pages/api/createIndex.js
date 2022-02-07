595901805c470b4c5a1af16a7cf1cba63bd3b4aa
m7mark <m7mark@yandex.ru>
2022-02-07 19:06:00 +0300

    createIndex

--- pages/api/createIndex.js
+++ pages/api/createIndex.js
@@ -1,6 +1,6 @@
 import { createIndex } from "../../lib/redis"
 
 export default async function handler(req, res) {
-  const id = await createIndex()
-  res.status(200).json('OK')
+  await createIndex()
+  res.status(200).send('OK')
 }
